69f2bf99aad3ef6b01e90936d4ec08a5a9229c6d
mathieuh64400 <mhedot@neuf.fr>
2021-11-04 14:43:26 +0100

    first commit

---
+++ test/index.test.js
@@ -0,0 +1,8 @@
+const assert =require('assert');
+const addFn =require("../index");
+describe('testAdd',function(){
+    it('should return 10',function(){
+         assert.equal(addFn.add(4,6),10)  ;
+    })
+ 
+})
