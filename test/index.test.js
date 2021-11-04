b80965a58d07232585cbe97dce687b661e051a36
mathieuh64400 <mhedot@neuf.fr>
2021-11-04 14:54:51 +0100

    error2

--- test/index.test.js
+++ test/index.test.js
@@ -2,7 +2,7 @@ const assert =require('assert');
 const addFn =require("../index");
 describe('testAdd',function(){
     it('should return 10',function(){
-         assert.equal(addFn.add(4,6),10)  ;
+         assert.equal(addFn.add(4,6),11)  ;
     })
  
 })
